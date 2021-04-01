1ac62eab0fea84c97dc47a489f04a7693c4745b4
afzaal wasim <80407489+afzaalwasm@users.noreply.github.com>
2021-04-01 20:41:10 +0500

    Delete Item
    Delete item using sawp

--- app/src/main/java/com/wasim/recycleviewdp/MainActivity.kt
+++ app/src/main/java/com/wasim/recycleviewdp/MainActivity.kt
@@ -5,36 +5,60 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: TodoAdapter
+    val todoList = mutableListOf(
+        Todo("Follow androidDevs", false),
+        Todo("Follow androidDevs", false),
+        Todo("Follow androidDevs", true),
+        Todo("Follow androidDevs", false),
+        Todo("Follow androidDevs", false),
+        Todo("Follow androidDevs", true),
+        Todo("Follow androidDevs", true)
+    )
+    private val itemTouchClick = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                (todoList as ArrayList<Todo>).removeAt(viewHolder.adapterPosition)
+                adapter.notifyDataSetChanged()
+
+                //adapter.notifyItemChanged(viewHolder.adapterPosition)
+            }
+
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var todoList = mutableListOf(
-                Todo("Follow androidDevs", false),
-                Todo("Follow androidDevs", false),
-                Todo("Follow androidDevs", true),
-                Todo("Follow androidDevs", false),
-                Todo("Follow androidDevs", false),
-                Todo("Follow androidDevs", true),
-                Todo("Follow androidDevs", true)
 
-        )
-        val adapter = TodoAdapter(todoList)
+        adapter = TodoAdapter(this, todoList as ArrayList<Todo>)
+
         rvTodos.adapter = adapter
         rvTodos.layoutManager = LinearLayoutManager(this)
+        val callback = ItemTouchHelper(itemTouchClick)
+        callback.attachToRecyclerView(rvTodos)
+
         btnAddtodo.setOnClickListener {
             val title = editText.text.toString()
             val todo = Todo(title, false)
             todoList.add(todo)
             adapter.notifyDataSetChanged()
             adapter.notifyItemInserted(todoList.size - 1)
-            rvTodos.scrollToPosition(todoList.size-1)
+            rvTodos.scrollToPosition(todoList.size - 1)
             editText.setText("")
             hideKeyboard(editText)
         }
@@ -42,7 +66,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun hideKeyboard(view: View) {
-        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputMethodManager =
+            getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
+
     }
 }
--- app/src/main/java/com/wasim/recycleviewdp/TodoAdapter.kt
+++ app/src/main/java/com/wasim/recycleviewdp/TodoAdapter.kt
@@ -1,16 +1,22 @@
 package com.wasim.recycleviewdp
 
 import android.app.Activity
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_todo.view.*
+import java.util.ArrayList
+import kotlin.math.log
 
 class TodoAdapter(
-    var todos: List<Todo>
+    val ctx: Context,
+    var todos: ArrayList<Todo>
 ) : RecyclerView.Adapter<TodoAdapter.TodoviewHolder>() {
 
     inner class TodoviewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
@@ -31,6 +37,19 @@ class TodoAdapter(
         holder.itemView.apply {
             tvTitle.text = todos[position].title
             cbDone.isChecked = todos[position].isChecked
+            imageView.setImageResource(R.drawable.ic_launcher_background)
+            tvTitle.setOnClickListener {
+                Log.d("tvTitle", "onBindViewHolder: ${tvTitle.text}")
+                Toast.makeText(ctx, "${tvTitle.text}", Toast.LENGTH_SHORT).show()
+            }
+//            tvTitle.setOnLongClickListener{
+//                todos.removeAt(position)
+//                notifyDataSetChanged()
+//                Toast.makeText(ctx, "Long click detected", Toast.LENGTH_SHORT).show()
+//                true
+//            }
+
+
         }
 
     }
